6a2b7108a29451b6c1e5cfced358e3d0a34958ac
Rapha-tn4 <raphamello49@gmail.com>
2026-05-06 17:10:16 +0200

    Java

---
+++ boucle.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+for(let i=100; i>0; i=i-2){
+    console.log(i)
+}
+
+
+
+
+//role : afficher la table de multi d'un nombre passé
+//parametre : nombre 
+
+
+
+
+
+
+function tablemulti(nombre){
+    for(let i=1; i<=10; i++){
+        console.log(`${nombre} x ${i} = ${nombre * i}`)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- conditions.js
+++ conditions.js
@@ -20,28 +20,28 @@ let b = 8;
 let c = 6;
 
 
-if(b === c){
+if (b === c) {
     console.log("bien joue")
-    
-} else{
+
+} else {
     console.log("dommage")
 }
 
 
-if ( b === c){
+if (b === c) {
     console.log("cest pareil")
-} else if  (b > c){
+} else if (b > c) {
     console.log("b est superieur a c")
-}else {
+} else {
     console.log("b est inferieur a c")
 }
 
 
 let connect = true;
 
-if(connect === true) {
+if (connect === true) {
     console.log("le ceme est co")
-} else{
+} else {
     console.log("le ceme est pas co")
 }
 
@@ -51,7 +51,7 @@ if(connect === true) {
 
 
 
-if (nbr % 2 === 0){
+if (nbr % 2 === 0) {
     console.log("nombre pair ! ")
 } else {
     console.log("impair")
@@ -66,21 +66,21 @@ let meteo = "neige";
 
 
 
-if("meteo" === "pluie" ){
+if ("meteo" === "pluie") {
     console.log("prends ton parapluie")
-} 
-else if("meteo" == "soleil"){
-        console.log("prends une casquette")
-    } else if{
-        console.log("prends tes bottes")
-    }
+}
+else if ("meteo" == "soleil") {
+    console.log("prends une casquette")
+} else if{
+    console.log("prends tes bottes")
+}
 
 
 
-    if(age< 12){
+    if (age < 12) {
         console.log("enfant");
     }
-    else if  (age>18){
+    else if (age > 18) {
         console.log("adulte")
     }
 
@@ -88,16 +88,133 @@ else if("meteo" == "soleil"){
 
 
 
+let a =
 
-
-if( a === b && b === c && a===c){
+if(a === b && b === c && a === c) {
     console.log("le triangle est equilaterales")
 }
-else if (a != b && b != c && a!=c){
+else if (a != b && b != c && a != c) {
     console.log("le triangle est quelcqonque")
 }
-else if 
-console.log("le triangle est isocèle")
+else {
+    console.log("le triangle est isocèle")
+}
+
+
+
+
+
+
+if (age >= 18 && membre fidelite || achat100) {
+    console.log("Réduction accordée")
+}
+else {
+    console.log("Réduction refusée")
+}
+
+
+function reduction(age, membre, achat) {
+    // si la personne a plus de 18ans
+
+    if (age > 18) {
+        // si la personne ou a fait plus de de 100
+        if (membre === true || achat > 100) {
+            //  reduction accordee
+            return "reduction accordee"
+        } else { // sinon :
+            // reduction refusee
+            return "reduction refusee"
+
+        }
+
+
+
+    } else {
+        // sinon :
+        // reduction refusee
+        return "reduction refusse : t'as pas 18 ans"
+    }
+
+
+
+
+
+
+
+
+function questionfeu(reponse){
+    let reponse =prompt("quel est la couleur du feu");
+    
+    if(reponse === rouge){
+        console.log("Je m'arrette")
+    
+    }else if(reponse === orange){
+        console.log("Je freine !")
+    
+    }else if(reponse === vert){
+        console.log("Je passe")
+    
+    }else (reponse === autre){
+        console.log("Danger : feu défectueux !")
+    }
+
+   
+}
+
+
+let moyennesglo = (a,b,c,d);
+
+
+// calcul moyenne globale
+
+function calculcmoyenneglo(a,b,c,d){
+    return (a+b+c+d)/4;
+}
+let moyg= calculcmoyenneglo(12,11,18,9)
+
+
+
+
+// Commentaire
+
+function commentaire(moyennesglo){
+    if(moyennesglo >= 0 && moyennesglo <= 10 ){
+        console.log( "Il faut travailler plus" )
+    
+    }else if(moyennesglo >=10 && moyennesglo <=13){
+        console.log("Poursuivez vos efforts")
+    
+    }else if(moyennesglo >=13 && moyennesglo <=15){
+        console.log("Bon travail")
+    
+    }else if(moyennesglo >=15 && moyennesglo <=17){
+        console.log("Bravo ! ")
+    }
+    else if(moyennesglo >17){
+        console.log("Félicitations !")
+    }
+
+}
+
+
+
+
+
+
+// Bulletin 
+
+// role : afficher le bulletin
+//parametre : la moyenneglo et le commentaire
+//return: l'affichage du bulletin
+
+
+
+function bulletin(moyennesglo,commentaire){
+    return (moyennesglo+" "+commentaire);
+
+}
+
+console.log(bulletin)
 
 
 
